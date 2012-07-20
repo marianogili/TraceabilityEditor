652ab7f603eed4065eb7226c6965b64d1aa3c66b
Mariano Gili <marianogili@gmail.com>
2012-07-20 19:49:43 -0300

    Primera propuesta de Perspectiva

--- TraceEditor/src/traceeditor/PerspectiveTraceEditorFactory.java
+++ TraceEditor/src/traceeditor/PerspectiveTraceEditorFactory.java
@@ -1,6 +1,5 @@
 package traceeditor;
 
-import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
@@ -8,22 +7,16 @@ public class PerspectiveTraceEditorFactory implements IPerspectiveFactory {
 
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
-		// TODO Auto-generated method stub
-		
-		// Editors are placed for free.
-        String editorArea = layout.getEditorArea();
 
-        // Place navigator and outline to left of
-        // editor area.
-        IFolderLayout left =
-                layout.createFolder("left", IPageLayout.LEFT, (float) 0.20, editorArea);
-        left.addView(IPageLayout.ID_PROJECT_EXPLORER);
-        IFolderLayout bottom =
-            layout.createFolder("bottom", IPageLayout.BOTTOM, (float) 0.20, editorArea);
-        bottom.addView(IPageLayout.ID_OUTLINE);
-        IFolderLayout leftbottom =
-            layout.createFolder("leftbottom", IPageLayout.BOTTOM, (float) 0.20, editorArea);
-        leftbottom.addView(IPageLayout.ID_PROBLEM_VIEW);
+		String editorArea = layout.getEditorArea();
+        
+        layout.addView(IPageLayout.ID_PROJECT_EXPLORER, IPageLayout.LEFT, (float) 0.20, editorArea);
+        layout.addView(IPageLayout.ID_OUTLINE, IPageLayout.BOTTOM, (float) 0.50, IPageLayout.ID_PROJECT_EXPLORER);
+        
+        layout.addView(IPageLayout.ID_PROP_SHEET, IPageLayout.BOTTOM, (float) 0.75, editorArea);
+        
+        layout.addView(IPageLayout.ID_PROBLEM_VIEW, IPageLayout.RIGHT, (float) 0.75, editorArea);
+        
 	}
 
 }
