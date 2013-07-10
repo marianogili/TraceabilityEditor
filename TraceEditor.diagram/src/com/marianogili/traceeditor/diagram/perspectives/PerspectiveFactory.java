9525d76d02da7bf52744890b2b4866f5a9718767
Mariano Gili <marianogili@gmail.com>
2013-07-10 18:09:44 -0300

    Perspectiva del editor definida, pero falta asociarle la vista del editor tabular que voy a cambiar.

---
+++ TraceEditor.diagram/src/com/marianogili/traceeditor/diagram/perspectives/PerspectiveFactory.java
@@ -0,0 +1,28 @@
+package com.marianogili.traceeditor.diagram.perspectives;
+
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class PerspectiveFactory implements IPerspectiveFactory {
+
+	private static final String FOLDER = 
+			"com.marianogili.traceeditor.diagram.perspectives.bottomFolder";
+	
+	private static final String VIEW_ID = "";
+
+	@Override
+	public void createInitialLayout(IPageLayout myLayout) {
+		myLayout.addView(IPageLayout.ID_PROJECT_EXPLORER, IPageLayout.LEFT, 0.20f,
+				myLayout.getEditorArea());
+		
+		myLayout.addView(IPageLayout.ID_OUTLINE, IPageLayout.BOTTOM, 0.6f, 
+				IPageLayout.ID_PROJECT_EXPLORER);
+		
+		IFolderLayout bot = myLayout.createFolder(FOLDER, IPageLayout.BOTTOM,
+				0.65f, myLayout.getEditorArea());
+		bot.addView(IPageLayout.ID_PROP_SHEET);
+		
+	}
+
+}
